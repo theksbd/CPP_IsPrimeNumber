2ee11df290f5c3a4c58f1de19f9aadfa14150639
Đỗ Thiện Hoàng <93083446+theksbd@users.noreply.github.com>
2022-04-28 16:03:45 +0700

    add execution time measuring

--- main.cpp
+++ main.cpp
@@ -1,26 +1,35 @@
 #include <iostream>
+#include <time.h>
+#include <iomanip>
 #include <math.h>
 
 using namespace std;
 
-bool IsPrime (int n)
+bool isPrime(long long n)
 {
     if (n < 2)
         return false;
-    if (n % 2 == 0 && n != 2)
+    if (n == 2)
+        return true;
+    if (n & 1 == 0) // even number
         return false;
-    for (int i = 3; i <= sqrt(n); i += 2)
+    for (long long i = 3; i <= sqrt(n); i++)
     {
         if (n % i == 0)
             return false;
     }
-    
     return true;
 }
 
 int main()
 {
-    cout << IsPrime(9);
-
+    // n = 10, 107, 1003, 12345, 138978, 2147483647, 6620830889
+    clock_t start_time, end_time;
+    start_time = clock();
+    isPrime(6620830889) ? cout << "Prime\n" : cout << "Not Prime\n";
+    end_time = clock();
+    double duration = double(end_time - start_time) / double(CLOCKS_PER_SEC);
+    cout << fixed;
+    cout << "Duration : " << setprecision(5) << duration << " (s)\n";
     return 0;
 }
